ca44e0d93deffdf7b3b158c0adc60ee257e1e7c4
erikvella <vella.erik1@gmail.com>
2023-09-14 13:38:21 +0100

    [init] while

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// 1 Prendo i miei elementi 
+
+// 2 creo un ciclo while che non si interrompe finchè non ha stampato tutti i prodotti
+
+
+
+
+
+const listaSpesa = ['latte' , 'sale' , 'zucchero' , 'pane'];
+let i = 0;
+
+while(i < listaSpesa.length){
+  console.log(listaSpesa[i]);
+  i++;
+}
+
+
